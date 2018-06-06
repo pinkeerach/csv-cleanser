c5cf0d7ccc21f1e54e00387dc537b09735a71c64
Pinkeerach (Rachel Parsons) <madethelflint@gmail.com>
2018-06-05 20:28:01 -0400

    tinkering with some alternative approaches with the csv library; no love yet

--- cleanse-csv.py
+++ cleanse-csv.py
@@ -1,17 +1,25 @@
 
 
-def attemptToParseFile():
+def parseFileWithCsvLibrary():
     import csv, sys
     filename = 'sample-with-broken-utf8.csv'
+    # filename = 'sample.csv'
     with open(filename, newline='', encoding='utf-8') as f:
-        reader = csv.reader(f, delimiter=',')
+        reader = csv.reader(f,strict='false')
         try:
             for row in reader:
-                print(row)
+                record = (x.encode('utf-8', errors='ignore').decode() for x in row)
+                yield record
         except UnicodeDecodeError as ue: 
             # bad stuff happened
             sys.exit('bad unicode character at position {}'.format(ue.start))
         except csv.Error as e:
             sys.exit('file {}, line {}: {}'.format(filename, reader.line_num, e))
 
-attemptToParseFile()
+# def handleBadEncoding():
+#     import 
+
+generator = parseFileWithCsvLibrary()
+
+for i in generator:
+    print(i)
