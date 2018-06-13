6207470addeb8bf25191f91ff1102ebcdbbbf63c
Pinkeerach (Rachel Parsons) <madethelflint@gmail.com>
2018-06-12 22:10:15 -0400

    fixed the delimiter splitting logic and now have correctly parsed fields and records

--- cleanse-csv.py
+++ cleanse-csv.py
@@ -1,34 +1,16 @@
 #!/usr/bin/env python
 
-def parseFileWithCsvLibrary():
-    import csv, sys
-    filename = 'sample-with-broken-utf8.csv'
-    # filename = 'sample.csv'
-    with open(filename, newline='', encoding='utf-8') as f:
-        reader = csv.reader(f,strict='false')
-        try: # TODO refactor this back to basic happy path parsing
-            for row in reader:
-                record = (x.encode('utf-8', errors='ignore').decode() for x in row)
-                yield record
-        except UnicodeDecodeError as ue: # TODO raise the exception
-            # bad stuff happened
-            sys.exit('bad unicode character at position {}'.format(ue.start))
-        except csv.Error as e:
-            sys.exit('file {}, line {}: {}'.format(filename, reader.line_num, e))
-
 def formatData(fieldData, columnIndex):
-    '''Timestamp,Address,ZIP,FullName,FooDuration,BarDuration,TotalDuration,Notes'''
+    # header guide: Timestamp,Address,ZIP,FullName,FooDuration,BarDuration,TotalDuration,Notes
+    import datetime
 
-    decodedString = str(fieldData, 'utf-8', 'replace')
-
-    stringToReturn = decodedString
+    stringToReturn = fieldData #default, do nothing
 
     if columnIndex == 0:
-        import datetime
         #format timestamp: ISO-8601 format, convert Pacific -> Eastern
-        datetimeFromField = datetime.datetime.strptime(decodedString, '%m/%d/%y %I:%M:%S %p')
+        datetimeFromField = datetime.datetime.strptime(stringToReturn, '%m/%d/%y %I:%M:%S %p')
         stringToReturn = datetimeFromField.strftime('%Y-%m-%d %H:%M:%S')
-    # elif columnIndex == 1 - format address; unicode validation only
+
     elif columnIndex == 2:
         #format zip; 5 chars, less than 5 digits, assume 0 as the prefix
         while (len(stringToReturn) < 5):
@@ -36,10 +18,15 @@ def formatData(fieldData, columnIndex):
             
     elif columnIndex == 3:
         #format fullname: uppercase, nonenglish
-        stringToReturn = decodedString.upper()
-    elif columnIndex == 4 or columnIndex == 5 or columnIndex == 5: 
+        stringToReturn = stringToReturn.upper()
+
+    elif columnIndex == 4 or columnIndex == 5: 
         #format duration; HH:MM:SS.MS format (where MS is milliseconds); please convert them to a floating point seconds format
-        stringToReturn = decodedString
+        try:
+            timeObject = datetime.datetime.strptime(stringToReturn, '%I:%M:%S.%f')
+            stringToReturn = timeObject.timestamp() * 1000
+        except ValueError:
+            stringToReturn = 0
 
     print(stringToReturn)
     return(stringToReturn)
@@ -47,38 +34,53 @@ def formatData(fieldData, columnIndex):
 def manuallyParseCsv(filename):
     
     with open(filename, 'rb') as csvFile: # read as binary to check for encoding
-        header = csvFile.readline()
-        
-        try: #not sure i need this anymore
+        header = csvFile.readline()        
+        try: 
             headerString = header.decode()
             columns = headerString.split(',')
-            '''Timestamp,Address,ZIP,FullName,FooDuration,BarDuration,TotalDuration,Notes'''
+            # 0 Timestamp, 1 Address, 2 ZIP, 3 FullName, 4 FooDuration, 5 BarDuration, 6 TotalDuration, 7 Notes
         except UnicodeDecodeError:
             return '''if we can't get a header, the rest of this approach falls apart.'''
 
-        next(csvFile)
+        if len(columns) != 8:
+            return # unexpected number of columns
 
         for line in csvFile:
-            # print(line)
-            fieldBytes = line.split(b',')
+            import re
+            regExPattern = '''(?:^|,)("(?:[^"]+|"")*"|[^,]*)'''
+            decodedLine = str(line, 'utf-8', 'replace')
+            splitDecodedLine = re.compile(regExPattern).split(decodedLine)
+
+            # a bit of a hack because my regex is rusty and i'm a python newb
+            decodedFields = []
+            for item in splitDecodedLine:
+                if item != '':
+                    decodedFields.append(item)
 
             fieldCounter = 0
             fieldList = []
-            for field in fieldBytes:    # apply formatting logic
+            totalDuration = 0
+
+            for field in decodedFields:    # apply formatting logic
                 # print(field) 
-                formattedField = formatData(field, fieldCounter)
-                fieldList.append(formattedField)
+                if fieldCounter != 6:
+                    formattedField = formatData(field, fieldCounter)
+                else:
+                    formattedField = totalDuration
+                
+                #  - add foo + bar durations = totalduration
+                if fieldCounter == 4:
+                    totalDuration = formattedField
+                elif fieldCounter == 5:
+                    totalDuration += formattedField
+
                 fieldCounter += 1
+                fieldList.append(formattedField)
         
-            # print(fieldList)
-            # business logic to apply:
-            #  - add foo + bar durations = total
-
-
 
 def parseCsvFile():
-    filename = 'sample-with-broken-utf8.csv'
-    # filename = 'sample.csv'
+    # filename = 'sample-with-broken-utf8.csv'
+    filename = 'sample.csv'
     with open(filename, 'r') as csvFile:
         try: # TODO refactor out to use the csv library and make that method throw an exception
             header = csvFile.readline()
